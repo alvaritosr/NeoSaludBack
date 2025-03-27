a635e19b1ca2bd341f6cae83ff73dc6265da1c30
alvaritosr <asanchezrubio@safareyes.es>
2025-03-27 11:45:00 +0100

    Primer Commit

---
+++ src/main/java/com/neo/ssalud/NeoSaludApplication.java
@@ -0,0 +1,13 @@
+package com.neo.ssalud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoSaludApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeoSaludApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neo/ssalud/NeoSaludApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neo.ssalud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoSaludApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
