059cae71365136fe4190293c2776bbca593b5a27
jcanterla <jcanterlarufino@safareyes.es>
2025-06-04 19:20:30 +0200

    Ingresos service y controller

---
+++ src/main/java/com/neo/ssalud/repositories/IngresosRepository.java
@@ -0,0 +1,9 @@
+package com.neo.ssalud.repositories;
+
+import com.neo.ssalud.models.Ingresos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IngresosRepository extends JpaRepository<Ingresos, Long> {
+
+
+}
--- src/main/java/com/neo/ssalud/repositories/habitoDeVidaRepository.java
+++ src/main/java/com/neo/ssalud/repositories/habitoDeVidaRepository.java
@@ -3,7 +3,6 @@ package com.neo.ssalud.repositories;
 import com.neo.ssalud.models.habitoDeVida;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Arrays;
 import java.util.List;
 
 public interface habitoDeVidaRepository extends JpaRepository<habitoDeVida, Long> {
