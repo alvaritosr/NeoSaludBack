e8cb5c8aec29c9a968be697ef1e6ff390be8ba56
alvaritosr <asanchezrubio@safareyes.es>
2025-06-11 00:14:33 +0200

    Render

--- src/main/java/com/neo/ssalud/security/CustomCorsConfiguration.java
+++ src/main/java/com/neo/ssalud/security/CustomCorsConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.List;
+
 
 @Configuration
 public class CustomCorsConfiguration  {
@@ -13,12 +15,18 @@ public class CustomCorsConfiguration  {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOriginPattern("*");
-        corsConfiguration.addAllowedMethod("*"); // Permitir todos los métodos (GET, POST, etc.)
-        corsConfiguration.addAllowedHeader("*"); // Permitir todos los encabezados
-        corsConfiguration.setAllowCredentials(true); // Si usas cookies o sesiones
-        corsConfiguration.addAllowedOrigin("http://localhost:4200");
+        List<String> allowedOrigins = List.of("https://neosaludfront.onrender.com", "https://localhost", "capacitor://localhost", "http://localhost:4200");
+        for (String origin : allowedOrigins) {
+            corsConfiguration.addAllowedOrigin(origin);
+        }
 
+        List<String> allowedMethods = List.of("GET", "POST", "PUT", "DELETE", "OPTIONS");
+        for (String method : allowedMethods) {
+            corsConfiguration.addAllowedMethod(method);
+        }
+
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
         return new CorsFilter(source);
