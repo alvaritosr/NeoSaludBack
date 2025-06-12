3a689af61ee31756dd88829ee73079e97f9243c9
jcanterla <jcanterlarufino@safareyes.es>
2025-06-12 23:48:06 +0200

    ResultadoAnalisis

--- src/main/java/com/neo/ssalud/security/WebSocketConfig.java
+++ src/main/java/com/neo/ssalud/security/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:4200") // Allow this origin
+                .setAllowedOrigins("https://neosaludback.onrender.com") // Allow this origin
                 .withSockJS();
     }
 }
