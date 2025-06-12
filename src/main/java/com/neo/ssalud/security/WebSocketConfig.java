2720e95b0eeb0701a613624c349cd43d36debafa
jcanterla <jcanterlarufino@safareyes.es>
2025-06-13 00:05:53 +0200

    front

--- src/main/java/com/neo/ssalud/security/WebSocketConfig.java
+++ src/main/java/com/neo/ssalud/security/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("https://neosaludback.onrender.com") // Allow this origin
+                .setAllowedOrigins("https://neosaludfront.onrender.com")
                 .withSockJS();
     }
 }
